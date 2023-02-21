236bbe357d2789456505962cfb716af6ca9873b1
Alexandre Messuve <alexandre.messuves@gmail.com>
2023-02-21 10:30:39 +0100

    Ajout du sql

--- boite_a_jurons.sql
+++ boite_a_jurons.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : lun. 20 fév. 2023 à 15:33
+-- Généré le : mar. 21 fév. 2023 à 10:05
 -- Version du serveur : 10.4.27-MariaDB
 -- Version de PHP : 7.4.33
 
@@ -28,8 +28,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `balance` (
-  `identifiant` int(11) NOT NULL,
-  `Login` varchar(50) NOT NULL,
+  `id_jurons` int(11) NOT NULL,
+  `login` varchar(50) NOT NULL,
   `Date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -40,10 +40,20 @@ CREATE TABLE `balance` (
 --
 
 CREATE TABLE `jurons` (
-  `identifiant` int(11) NOT NULL,
+  `id_jurons` int(11) NOT NULL,
   `categorie` varchar(50) NOT NULL,
-  `Prix` float NOT NULL
+  `prix` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `jurons`
+--
+
+INSERT INTO `jurons` (`id_jurons`, `categorie`, `prix`) VALUES
+(1, 'petite', 0.1),
+(2, 'grande', 0.3),
+(3, 'roter', 0.5),
+(4, 'geste', 0.7);
 
 -- --------------------------------------------------------
 
@@ -52,8 +62,8 @@ CREATE TABLE `jurons` (
 --
 
 CREATE TABLE `peut_etre` (
-  `Identifiant` int(11) NOT NULL,
-  `Login` varchar(50) NOT NULL,
+  `id_retard` int(11) NOT NULL,
+  `login` varchar(50) NOT NULL,
   `Date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -64,9 +74,16 @@ CREATE TABLE `peut_etre` (
 --
 
 CREATE TABLE `retard` (
-  `Identifiant` int(11) NOT NULL,
-  `Prix` float NOT NULL
+  `id_retard` int(11) NOT NULL,
+  `prix` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `retard`
+--
+
+INSERT INTO `retard` (`id_retard`, `prix`) VALUES
+(1, 0.1);
 
 -- --------------------------------------------------------
 
@@ -75,9 +92,16 @@ CREATE TABLE `retard` (
 --
 
 CREATE TABLE `role` (
-  `Identifiant` int(11) NOT NULL,
+  `id_role` int(11) NOT NULL,
   `type` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `role`
+--
+INSERT INTO `role` (`id_role`, `type`) VALUES
+(1, 'Admin'),
+(2, 'Normal');
 
 -- --------------------------------------------------------
 
@@ -86,12 +110,12 @@ CREATE TABLE `role` (
 --
 
 CREATE TABLE `utilisateur` (
-  `Login` varchar(50) NOT NULL,
+  `login` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `nom` varchar(50) NOT NULL,
-  `Prenom` varchar(50) NOT NULL,
-  `Date_de_Naissance` varchar(50) NOT NULL,
-  `Identifiant` int(11) NOT NULL
+  `prenom` varchar(50) NOT NULL,
+  `date_de_Naissance` varchar(50) NOT NULL,
+  `id_role` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -102,40 +126,40 @@ CREATE TABLE `utilisateur` (
 -- Index pour la table `balance`
 --
 ALTER TABLE `balance`
-  ADD PRIMARY KEY (`identifiant`,`Login`),
-  ADD KEY `Balance_utilisateur0_FK` (`Login`);
+  ADD PRIMARY KEY (`id_jurons`,`login`),
+  ADD KEY `Balance_utilisateur0_FK` (`login`);
 
 --
 -- Index pour la table `jurons`
 --
 ALTER TABLE `jurons`
-  ADD PRIMARY KEY (`identifiant`);
+  ADD PRIMARY KEY (`id_jurons`);
 
 --
 -- Index pour la table `peut_etre`
 --
 ALTER TABLE `peut_etre`
-  ADD PRIMARY KEY (`Identifiant`,`Login`),
-  ADD KEY `Peut_etre_utilisateur0_FK` (`Login`);
+  ADD PRIMARY KEY (`id_retard`,`login`),
+  ADD KEY `Peut_etre_utilisateur0_FK` (`login`);
 
 --
 -- Index pour la table `retard`
 --
 ALTER TABLE `retard`
-  ADD PRIMARY KEY (`Identifiant`);
+  ADD PRIMARY KEY (`id_retard`);
 
 --
 -- Index pour la table `role`
 --
 ALTER TABLE `role`
-  ADD PRIMARY KEY (`Identifiant`);
+  ADD PRIMARY KEY (`id_role`);
 
 --
 -- Index pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  ADD PRIMARY KEY (`Login`),
-  ADD KEY `utilisateur_role_FK` (`Identifiant`);
+  ADD PRIMARY KEY (`login`),
+  ADD KEY `utilisateur_role_FK` (`id_role`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -145,19 +169,19 @@ ALTER TABLE `utilisateur`
 -- AUTO_INCREMENT pour la table `jurons`
 --
 ALTER TABLE `jurons`
-  MODIFY `identifiant` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_jurons` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `retard`
 --
 ALTER TABLE `retard`
-  MODIFY `Identifiant` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_retard` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `role`
 --
 ALTER TABLE `role`
-  MODIFY `Identifiant` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_role` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Contraintes pour les tables déchargées
@@ -167,21 +191,21 @@ ALTER TABLE `role`
 -- Contraintes pour la table `balance`
 --
 ALTER TABLE `balance`
-  ADD CONSTRAINT `Balance_Jurons_FK` FOREIGN KEY (`identifiant`) REFERENCES `jurons` (`identifiant`),
-  ADD CONSTRAINT `Balance_utilisateur0_FK` FOREIGN KEY (`Login`) REFERENCES `utilisateur` (`Login`);
+  ADD CONSTRAINT `Balance_Jurons_FK` FOREIGN KEY (`id_jurons`) REFERENCES `jurons` (`id_jurons`),
+  ADD CONSTRAINT `Balance_utilisateur0_FK` FOREIGN KEY (`login`) REFERENCES `utilisateur` (`login`);
 
 --
 -- Contraintes pour la table `peut_etre`
 --
 ALTER TABLE `peut_etre`
-  ADD CONSTRAINT `Peut_etre_Retard_FK` FOREIGN KEY (`Identifiant`) REFERENCES `retard` (`Identifiant`),
-  ADD CONSTRAINT `Peut_etre_utilisateur0_FK` FOREIGN KEY (`Login`) REFERENCES `utilisateur` (`Login`);
+  ADD CONSTRAINT `Peut_etre_Retard_FK` FOREIGN KEY (`id_retard`) REFERENCES `retard` (`id_retard`),
+  ADD CONSTRAINT `Peut_etre_utilisateur0_FK` FOREIGN KEY (`login`) REFERENCES `utilisateur` (`login`);
 
 --
 -- Contraintes pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  ADD CONSTRAINT `utilisateur_role_FK` FOREIGN KEY (`Identifiant`) REFERENCES `role` (`Identifiant`);
+  ADD CONSTRAINT `utilisateur_role_FK` FOREIGN KEY (`id_role`) REFERENCES `role` (`id_role`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
