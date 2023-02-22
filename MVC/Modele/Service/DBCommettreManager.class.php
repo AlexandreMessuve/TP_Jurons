216bc6fb75fb45491036b03281c9ef3e463c74bd
59013-90-03 <59013-90-03@exchange.ad.afpanet>
2023-02-22 15:47:21 +0100

    Modification classes

--- MVC/Modele/Service/DBCommettreManager.class.php
+++ MVC/Modele/Service/DBCommettreManager.class.php
@@ -7,6 +7,41 @@ class DBCommettreManager {
         return new PDO('mysql:host=localhost;dbname=boite_a_jurons', 'root', '');
     }
 
+        //Method static qui permet d'inserer une infraction dans la bdd
+        static function insertPenalite(Commettre $commettre): bool
+        {
+            $codeInfraction = $commettre->getCodeInfraction();
+            $loginUtilisateur = $commettre->getLoginUtilisateur();
+            $loginBalance = $commettre->getLoginBalance();
+            $pdo = self::PDO();
+            $sql = "INSERT INTO commettre (`code_infraction`, `login_utilisateur`, `login_balance`) 
+                    VALUES (?,?,?)";
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(1, $codeInfraction);
+            $stmt->bindParam(2, $loginUtilisateur);
+            $stmt->bindParam(3, $loginBalance);
+            return $stmt->execute();
+        }
     
+        static function deletePenalite(Commettre $commettre): bool
+        {
+    
+            $codeInfraction = $commettre->getCodeInfraction();
+            $pdo = self::PDO();
+            $sql = "DELETE FROM `utilisateur` WHERE code_infraction =$codeInfraction";
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(1, $login);
+            return  $stmt->execute();
+        }
+
+        static function selectPenalite(): array
+        {
+            $pdo = self::PDO();
+            $sql = "SELECT * FROM commettre";
+            $stmt = $pdo->query($sql);
+            return $stmt->fetchAll(PDO::FETCH_OBJ);
+        }
+
+       
 
 }
