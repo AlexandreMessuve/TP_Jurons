470c8596cf0a375639ad9b7458811bbf9b3d9483
59013-90-16 <59013-90-16@exchange.ad.afpanet>
2023-02-22 16:07:01 +0100

    login update

--- MVC/View/login.php
+++ MVC/View/login.php
@@ -1,14 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html>  
+ <html>  
+      <head>  
+           <title>Login</title>  
+           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+      </head>  
+      <body>  
+           <br />  
+           <div class="container" style="width:500px;">  
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style2.css">
-    <title>Document</title>
-</head>
-<body>
-    <h1>sfdsf</h1>
-</body>
-</head>
+                <h3> LOGIN </h3> <br />  
+                <form method="post" action="../Controller/executeLogin.php">  
+                     <label>Username</label>  
+                     <input type="text" name="login" class="form-control" />  
+                     <br />  
+                     <label>Password</label>  
+                     <input type="password" name="password" class="form-control" />  
+                     <br />  
+                     <input type="submit" name="login" class="btn btn-info" value="Login" />  
+                </form>  
+           </div>  
+           <br />  
+      </body>  
+ </html>  
