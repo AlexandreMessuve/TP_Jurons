5508d289d5160f26c191a7b6de1091ebcc2365b2
alexandre.messuves@gmail.com <alexandre.messuves@gmail.com>
2023-02-22 15:44:32 +0100

    Maj executeInscription.php & Utilisateur.class.php

--- MVC/Controller/executeInscription.php
+++ MVC/Controller/executeInscription.php
@@ -3,17 +3,16 @@ require_once '../Modele/Service/DBUtilisateurManager.class.php';
 require_once '../Modele/Service/DBRolesManager.class.php';
 require_once '../Modele/Utilisateur.class.php';
 require_once '../Modele/Roles.class.php';
+$role = DBRolesManager::selectRoles();
+$id_role = $role[1]->id_roles;
+$type_role = $role[1]->type_roles;
+$roles = new Roles($id_role, $type_role);
+$utilisateur = new Utilisateur($_POST, $roles);
 
+$status = DButilisateurManager::insertUtilisateur($utilisateur);
 
-
-
-
-
-
-$password = 'Pass';
-
-$keyhash = password_hash($password, PASSWORD_DEFAULT, ['cost' => 10]);
-
-echo $keyhash;
-
-var_dump(password_verify('Pass', $keyhash));
+if ($status){
+    header('Location: ../View/tabeleau.php');
+}else{
+    echo 'error';
+}
