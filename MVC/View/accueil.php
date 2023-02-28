c72278474f11e9ac85e9b6706339d7334de88d7a
Sebastien <leg.sebastien@gmail.com>
2023-02-28 13:32:45 +0100

    Modif style balance et accueil

--- MVC/View/accueil.php
+++ MVC/View/accueil.php
@@ -5,21 +5,93 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/style2.css">
+    <link rel="stylesheet" href="./assets/css/accueilStyle.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Document</title>
 </head>
 
 <body>
 
 
-<?php session_start() ?>
+    <?php session_start() ?>
 
-<div class="boutons">
-    <form enctype="multipart/form-data" action="../Controller/executeBalance.php" method="post">
-    <button id="bouton2" style="width: 30em;" type="submit">Voir le tableau des balances</button>
-    </form>
-    </div>
+    <nav class="navbar navbar-dark fixed-top" style="background-color: #000000">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Accueil</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
+                data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar"
+                aria-labelledby="offcanvasDarkNavbarLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Menu</h5>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"
+                        aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="balance.php">Le tableau des
+                                balances</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="profil.php">Mon profil</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Que souhaitez vous consulter ?
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><a class="dropdown-item" href="graphique.php">Le graphique des jurons</a></li>
+                                <li><a class="dropdown-item" href="tableau.php">Le tableau des utilisateurs</a></li>
+                                <li>
+                            </ul>
+                        </li>
+                    </ul>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </nav>
 
-    
+
+        <div class="logo">
+            <img src="./assets/img/boiteajurons.png" alt="La boite à jurons">
+        </div>
+
+        <!-- Jumbotron -->
+        <div class="p-5 text-center ">
+            <h1 class="mb-3">Bienvenue ! </h1>
+            <h4 class="mb-3">Merde, fais chier, putain de bordel de merde, etc... Toi aussi tu es un vrai coureur de
+                jurons ?</h4>
+            <div class="boutons">
+                <form enctype="multipart/form-data" action="../Controller/executeBalance.php" method="post">
+                    <button id="bouton2" style="width: 30em;" type="submit">Voir le tableau des balances</button>
+                </form>
+            </div>
+        </div>
+        <!-- Jumbotron -->
+
+
+
+
+        <footer class="text-center text-white fixed-bottom" style="background-color: #673ab7">
+            <!-- Grid container -->
+            <div class="container p-4"></div>
+            <!-- Grid container -->
+
+            <!-- Copyright -->
+            <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+                © 2023 Copyright:
+                <a class="text-white" href="https://www.afpa.fr/">Afpa.fr</a>
+            </div>
+            <!-- Copyright -->
+        </footer>
 </body>
-</head>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+    crossorigin="anonymous"></script>
--- MVC/View/balance.php
+++ MVC/View/balance.php
@@ -2,24 +2,26 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../View/assets/css/balanceStyle.css">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../View/assets/css/balanceStyle.css">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 
-<?php session_start ()?>
-<nav class="navbar navbar-dark bg-dark fixed-top">
+<?php session_start() ?>
+<nav class="navbar navbar-dark fixed-top" style="background-color: #000000">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">La liste des balances</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+    <a class="navbar-brand" href="#">Historique des jurons</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar"
+      aria-controls="offcanvasDarkNavbar">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar"
+      aria-labelledby="offcanvasDarkNavbarLabel">
       <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Dark offcanvas</h5>
+        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Menu</h5>
         <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body">
@@ -28,90 +30,71 @@
             <a class="nav-link active" aria-current="page" href="accueil.php">Accueil</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="profil.php">Profil</a>
+            <a class="nav-link" href="profil.php">Mon profil</a>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
+              Que souhaitez vous consulter ?
             </a>
             <ul class="dropdown-menu dropdown-menu-dark">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="graphique.php">Le graphique des jurons</a></li>
+              <li><a class="dropdown-item" href="tableau.php">Le tableau des utilisateurs</a></li>
               <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
             </ul>
           </li>
         </ul>
-        <form class="d-flex mt-3" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-success" type="submit">Search</button>
         </form>
       </div>
     </div>
   </div>
 </nav>
 
-<div class="parent">
-
-<div class="div1">
 
 
 
-</div>
+<table class="table table-striped">
+  <thead class="black white-text" style="background-color: #673ab7">
+    <tr>
+      <th scope="col">Code Infraction</th>
+      <th scope="col">Login utilisateur</th>
+      <th scope="col">Login balance</th>
+      <th scope="col">Date_infraction</th>
+    </tr>
+  </thead>
 
+  <?php for ($i = 0; $i < count($_SESSION["donneesCommettre"]); $i++) { ?>
 
+    <tr>
+      <td>
+        <?php echo $_SESSION['donneesCommettre'][$i]['code_infraction'] ?>
+      </td>
+      <td>
+        <?php echo $_SESSION['donneesCommettre'][$i]['login_utilisateur'] ?>
+      </td>
+      <td>
+        <?php echo $_SESSION['donneesCommettre'][$i]['login_balance'] ?>
+      </td><br>
+      <td>
+        <?php echo $_SESSION['donneesCommettre'][$i]['date_infraction'] ?>
+      </td><br>
+    </tr>
 
-
-
-
-
-<div class="div2"> 
-
-<table>
-
-  <tr>
-    <th><strong>Code Infraction </strong></th>
-    <th><strong>Login utilisateur</strong></th>
-    <th><strong>Login balance</strong></th> <br>
-    <th><strong>Date_infraction</strong></th> <br>
-
-  </tr>
-
-  <?php  for ($i = 0; $i < count($_SESSION["donneesCommettre"]); $i++)  {?>
-
-
-<tr>
-  <td><?php  echo $_SESSION['donneesCommettre'][$i]['code_infraction']?></td>
-  <td><?php  echo $_SESSION['donneesCommettre'][$i]['login_utilisateur']?></td>
-  <td><?php  echo $_SESSION['donneesCommettre'][$i]['login_balance']?></td><br>
-  <td><?php  echo $_SESSION['donneesCommettre'][$i]['date_infraction']?></td><br>
-</tr>
-
-<?php   } ?>
-</div>
-<div class="div3"> </div>
-<div class="div4"> </div>
-</div>
-
-
+  <?php } ?>
 
 </table>
+<footer class="text-center text-white fixed-bottom" style="background-color: #673ab7">
+  <!-- Grid container -->
+  <div class="container p-4"></div>
+  <!-- Grid container -->
 
-
-
-
-
-
-
-<div class="div2"> </div>
-<div class="div3"> </div>
-<div class="div4"> </div>
-</div>
-
-
-</table>
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2023 Copyright:
+    <a class="text-white" href="https://www.afpa.fr/">Afpa.fr</a>
+  </div>
+  <!-- Copyright -->
+</footer>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </head>
