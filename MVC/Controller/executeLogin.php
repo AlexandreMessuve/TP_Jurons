64c84152ecd199fdbcd8b738f9eb9284348b1091
alexandre.messuves@gmail.com <alexandre.messuves@gmail.com>
2023-03-01 14:05:11 +0100

    maj login

--- MVC/Controller/executeLogin.php
+++ MVC/Controller/executeLogin.php
@@ -1,5 +1,5 @@
-<?php 
-
+<?php
+session_start();
 require_once "../Modele/Service/DBLogin.class.php";
 require_once "../Modele/Service/DBUtilisateurManager.class.php";
 $login = $_POST['login'];
@@ -9,9 +9,7 @@ $statut = DBLogin:: authentification($login, $password);
 
 
 if ($statut) {
-    echo 'Successfully logged in';
-    session_start();
-    $_SESSION['login'] = DBUtilisateurManager::selectUtilisateurByLogin($login);
+    $_SESSION['currentUser'] = DBUtilisateurManager::selectUtilisateurByLogin($login);
     $_SESSION['users'] = DBUtilisateurManager::selectUtilisateur();
     header ('Location: ../View/tableau.php');
 } else {
