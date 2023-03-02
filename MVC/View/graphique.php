a8224c36c5e63177d74e309b32cccd8fb6aeff07
Legrand Sebastien <leg.sebastien@gmail.com>
2023-03-02 15:13:07 +0100

    Création graphique

--- MVC/View/graphique.php
+++ MVC/View/graphique.php
@@ -5,12 +5,52 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/style2.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/graphiqueStyle.css">
+    <title>Graphique</title>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
 </head>
+
+<?php session_start(); ?>
+
+<?php 
+$_SESSION["requete"];
+
+foreach($_SESSION["requete"] as $data){
+    $prenom[] = $data['prenom'];
+    $totalPrix[] = $data['totalPrix'];
+}
+
+
+?>
 
 <body>
+<div>
+  <canvas id="myChart"></canvas>
+</div>
 
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+      labels: <?php echo json_encode($prenom)?>,
+      datasets: [{
+        label: '# of Votes',
+        data: <?php echo json_encode($totalPrix)?>,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+
+      }
+    }
+  });
+</script>
+ 
 </body>
 </head>
-
