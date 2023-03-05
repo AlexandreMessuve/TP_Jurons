a8e659f0bf5bd8100ab7ebec778663bd6f4ae84c
Alexandre Messuve <alexandre.messuves@gmail.com>
2023-03-05 18:36:10 +0100

    maj bdd

--- MVC/View/graphique.php
+++ MVC/View/graphique.php
@@ -13,7 +13,7 @@
 
 </head>
 
-<body>
+<body onload="adminLoad()">
 
     <?php session_start(); ?>
 
@@ -165,6 +165,11 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
             crossorigin="anonymous"></script>
+    <script
+            src="https://code.jquery.com/jquery-3.6.3.min.js"
+            integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU="
+            crossorigin="anonymous"></script>
+    <script src="../View/assets/js/functions.js"></script>
 
 </body>
 
