366a6851197a5fda9ef3ba63ee83f08b91e9677b
ANTHONY CANIPEL <canipelanthony@hotmail.fr>
2023-03-05 19:03:18 +0100

    modi profils

--- MVC/Controller/executeProfilModif.php
+++ MVC/Controller/executeProfilModif.php
@@ -4,24 +4,24 @@ require_once "../Modele/service/DBRolesManager.class.php";
 require_once "../Modele/service/DBUtilisateurManager.class.php";
 require_once '../Modele/Utilisateur.class.php';
 require_once '../Modele/Roles.class.php';
-
+session_start();
 
 $id_role = $_SESSION['currentUser']->id_roles;
-$roles = new Roles($id_role, $type_role);
-
+$role = DBRolesManager::selectRolesByType($id_role); 
+$roles = new Roles($id_role, $role->type_roles);
+$login = $_SESSION['currentUser']->login_user; 
 
 
 
 if (isset($_POST)) {
     if (!empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['email'])
-        && !empty($_POST['date']) && !empty($_POST['login']) && !empty($_REQUEST['password'])) {
+        && !empty($_POST['date']) && !empty($login) && !empty($_REQUEST['password'])) {
         if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
             echo "Email address non valide";
         }
         $nom = strip_tags($_POST['nom']);
         $prenom = strip_tags($_POST['prenom']);
         $email = $_POST['email'];
-        $login = strip_tags($_POST['login']);
         $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
         $date = $_POST['date'];
         $utilisateur = new Utilisateur($nom, $prenom, $date, $login, $email, $password, $roles);
